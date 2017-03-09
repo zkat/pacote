c3dc481584edfe3fe201fc6a6d78fbe198f9abdc
Kat Marchán <kzm@sykosomatic.org>
2017-03-09 01:42:47 -0800

    chore(docs): converted to new style

--- test/docs.js
+++ test/docs.js
@@ -1,25 +1,26 @@
 'use strict'
 
-var fs = require('fs')
-var path = require('path')
-var test = require('tap').test
+const BB = require('bluebird')
 
-test('all fixtures are documented', function (t) {
+const fs = BB.promisifyAll(require('fs'))
+const path = require('path')
+const test = require('tap').test
+
+test('all fixtures are documented', t => {
   // TODO - actually parse that table and make sure the
   //        important bits are documented?
   var readmePath = path.join(__dirname, 'fixtures', 'README.md')
-  fs.readFile(readmePath, 'utf8', function (err, text) {
-    if (err) { throw err }
-    fs.readdir(path.dirname(readmePath), function (err, files) {
-      if (err) { throw err }
+  return BB.join(
+    fs.readFileAsync(readmePath, 'utf8'),
+    fs.readdirAsync(path.dirname(readmePath)),
+    (text, files) => {
       files.forEach(function (f) {
         if (f !== 'README.md') {
           t.match(text, f, f + ' is mentioned.')
         }
       })
-      t.end()
-    })
-  })
+    }
+  )
 })
 
 test('all toplevel api calls are documented')
